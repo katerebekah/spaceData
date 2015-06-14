b9dff4681cf7ef71371d27de5323c8284a9fe1ae
Kate <katerebekah@gmail.com>
2015-06-13 20:53:11 -0500

    Planets are Rotating!

--- js/spaceData.js
+++ js/spaceData.js
@@ -10,14 +10,14 @@
                 return response;
             });
         }
-        //getAPIdata();
+        getAPIdata();
 
     //Set the scene
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 200000);
+    var camera = new THREE.PerspectiveCamera(45, window.innerHeight / window.innerHeight, 0.1, 200000);
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerHeight, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     //Make Planets/Sun
@@ -30,12 +30,13 @@
         });
         var sphere = new THREE.Mesh(geometry, material);
         sphere.position.set(planetData.x, planetData.y, planetData.z);
+        sphere.v = planetData.v;
         return sphere;
     };
 
     //planet data
     var sun = {
-        r: 7, // radius 69.58 
+        r: 140, // radius 695,800 km
         x: 0, 
         y: 0,
         z: 0,
@@ -43,66 +44,74 @@
         img: "/img/sun.jpg"
     };
     var mercury = {
-        r: .244,
-        x: 57.9,
+        r: 2.44, // 2,440
+        x: 57.9 + sun.r,
         y: 0,
         z: 0,
+        s: 1, //orbit speed ratio
         v: new THREE.Vector3(0, 0, 47.4),
         img: "/img/mercury.jpg"
     };
     var venus = {
-    	r: .6052,
-    	x:108.2,
+    	r: 6.052,
+    	x:108.2 + sun.r,
     	y:0,
     	z:0,
+    	s:2.55,
     	v: new THREE.Vector3(0, 0, 35),
     	img: "/img/venus.jpg"
     };
     var earth = {
-    	r: .6371,
-    	x:149.6,
+    	r: 6.371,
+    	x:149.6 + sun.r,
     	y:0,
     	z:0,
+    	s:4.51,
     	v:new THREE.Vector3(0, 0, 39.8),
     	img: "/img/earth.jpg"
     };
     var mars = {
-    	r: .339,
-    	x:227.9,
+    	r: 3.39,
+    	x:227.9 + sun.r,
     	y:0,
     	z:0,
+    	s:7.81,
     	v:new THREE.Vector3(0, 0, 24.1),
     	img: "/img/mars.jpg"
     };
     var jupiter = {
-    	r: 6.9911,
-    	x:778.6,
+    	r: 69.911,
+    	x:778.6 + sun.r,
     	y:0,
     	z:0,
+    	s: 49.22,
     	v:13.1,
     	img: "/img/jupiter.jpg"
     };
     var saturn = {
-    	r: 5.8232,
-    	x:1433.5,
+    	r: 58.232,
+    	x:1433.5 + sun.r,
     	y:0,
     	z:0,
+    	s:122.13,
     	v:new THREE.Vector3(0, 0, 9.7),
     	img: "/img/saturn.jpg"
     };
     var uranus = {
-    	r: 2.5362,
-    	x:2872.5,
+    	r: 25.362,
+    	x:2872.5 + sun.r,
     	y:0,
     	z:0,
+    	s:347.6,
     	v:new THREE.Vector3(0, 0, 6.8),
     	img: "/img/Uranus.png"
     };
     var neptune = {
-    	r: 2.4622,
-    	x:4495.1,
+    	r: 24.622,
+    	x:4495.1 + sun.r,
     	y:0,
     	z:0,
+    	s:679.55,
     	v:new THREE.Vector3(0, 0, 5.4),
     	img: "/img/neptune.jpg"
     }
@@ -116,21 +125,17 @@
     var Uranus = makePlanets(uranus);
     var Neptune = makePlanets(neptune);
 
-    var planets = [];
-    planets.push(Mercury);
-    planet.push(Venus);
-    planet.push(Earth);
-    planet.push(Mars);
-    planet.push(Jupiter);
-    planet.push(Saturn);
-    planet.push(Uranus);
-    planet.push(Neptune);
-    //Set planets in orbit	
-
+    time = 0;
     function orbit(){
-    	for (var i = 0; i < planets.length; planets++){
-    		
-    	}
+    	time += .1;
+    	Mercury.position.set(mercury.x* Math.cos(time/mercury.s), mercury.x* Math.sin(time/mercury.s), 0);
+    	Venus.position.set(venus.x* Math.cos(time/venus.s), venus.x* Math.sin(time/venus.s), 0);
+    	Earth.position.set(earth.x* Math.cos(time/earth.s), earth.x* Math.sin(time/earth.s), 0);
+    	Mars.position.set(mars.x* Math.cos(time/mars.s), mars.x* Math.sin(time/mars.s), 0);
+    	Jupiter.position.set(jupiter.x*Math.cos(time/jupiter.s), jupiter.x*Math.sin(time/jupiter.s), 0);
+    	Saturn.position.set(saturn.x* Math.cos(time/saturn.s), saturn.x* Math.sin(time/saturn.s), 0);
+    	Uranus.position.set(uranus.x* Math.cos(time/uranus.s), uranus.x* Math.sin(time/uranus.s), 0);
+    	Neptune.position.set(neptune.x* Math.cos(time/neptune.s), neptune.x* Math.sin(time/neptune.s), 0);
     }
 
 
@@ -159,6 +164,13 @@
     //render scene
     scene.add(Sun);
     scene.add(Mercury);
+    scene.add(Venus);
+    scene.add(Earth);
+    scene.add(Mars);
+    scene.add(Jupiter);
+    scene.add(Saturn);
+    scene.add(Uranus);
+    scene.add(Neptune);
 
     camera.position.set(0,0,50);
 
@@ -172,7 +184,8 @@
     
           // Render the scene.
           renderer.render(scene, camera);
-          orbit();
+           orbit();
+
           controls.update();
 
     }
